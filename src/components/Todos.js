55d2101d94035f1a8d2e561387134f94dafade6e
Bibash Acharya <acharabibash@outlook.com>
2020-07-21 11:47:28 +0545

    Second Commit

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+
+class Todos extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Todos</h1>
+      </div>
+    )
+  }
 }
 
 export default Todos;
