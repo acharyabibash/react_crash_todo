2fb750b4f3a1106d8ec042bbd953b77bc725aefe
Bibash Acharya <acharabibash@outlook.com>
2020-07-26 11:49:23 +0545

    40.53 brad

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = ()=>{
+        return {
+            background : '#f4f4f4',
+            padding : '10px',
+            borderBottom : '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through':'none'
+        }
+    }    
+    
+    
     render() {
         return (
-            <div style={ itemStyle }>
-               <p>{ this.props.todo.title }</p> 
+            <div style={this.getStyle()}>
+               <p>
+                   <input type = "checkbox"/> {' '}
+                   { this.props.todo.title }
+                </p> 
             </div>
         )
     }
@@ -16,8 +29,5 @@ TodoItem.propTypes = {
     todo : PropTypes.object.isRequired
 }
 
-const itemStyle = {
-    backgroundColor : '#f4f4f4',
-}
 
 export default TodoItem;
