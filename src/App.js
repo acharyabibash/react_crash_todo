7b6011778ebad1d6cee59d0361f8ddb78c2dc309
Bibash Acharya <acharabibash@outlook.com>
2020-07-26 17:58:12 +0545

    1.14.20 brad

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -42,12 +43,23 @@ delTodo = (id) =>{
 }
 
 
+// Add Todo
+addTodo = (title) =>{
+  const newTodo = {
+    id: 4,
+    title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos,newTodo] });
+
+}
+
   render(){  
     return(
       <div className="App">
         <div className="container">
         <Header />
-        <AddTodo />
+        <AddTodo addTodo={this.addTodo}/>
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
         </div>       
       </div>
