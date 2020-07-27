363b38025fc928d2c6e41d6a5c70f49cd1f3b279
Bibash Acharya <acharabibash@outlook.com>
2020-07-27 11:09:54 +0545

    1.27.18 Brad

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React,{ Component } from 'react';
-import { BroswerRouter as Router,Route } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import {v4 as uuid} from 'uuid';
 
 
@@ -58,13 +59,21 @@ addTodo = (title) =>{
 
   render(){  
     return(
-      <div className="App">
-        <div className="container">
-        <Header />
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
-        </div>       
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+          <Header />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
+          
+          </div>       
+        </div>
+      </Router>
     )
   }
 }
